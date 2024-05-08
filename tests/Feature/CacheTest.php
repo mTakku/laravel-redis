32149c03ac0cdcb761161fb4420c4a952d2ab926
Takku <farelvivo06@gmail.com>
2024-05-08 11:07:10 +0700

    added cache2

--- tests/Feature/CacheTest.php
+++ tests/Feature/CacheTest.php
@@ -4,17 +4,26 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Cache;
 use Tests\TestCase;
 
 class CacheTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
+    public function testCache()
     {
-        $response = $this->get('/');
+        Cache::put("name", "Farel", 2);
+        Cache::put("country", "Indonesia", 2);
 
-        $response->assertStatus(200);
+        $response = Cache::get("name");
+        self::assertEquals("Farel", $response);
+        $response = Cache::get("country");
+        self::assertEquals("Indonesia", $response);
+
+        sleep(5);
+
+        $response = Cache::get("name");
+        self::assertNull($response);
+        $response = Cache::get("country");
+        self::assertNull($response);
     }
 }
