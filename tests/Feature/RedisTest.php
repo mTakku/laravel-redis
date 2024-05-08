44b0a5cc7cabd2e6147d7aa1fcf8b1af592fac67
Takku <farelvivo06@gmail.com>
2024-05-08 10:40:18 +0700

    added hash and geopoint

--- tests/Feature/RedisTest.php
+++ tests/Feature/RedisTest.php
@@ -50,4 +50,52 @@ class RedisTest extends TestCase
 
     }
 
+    public function testSet()
+    {
+        Redis::del("names");
+
+        Redis::sadd("names", "Farel");
+        Redis::sadd("names", "Farel");
+        Redis::sadd("names", "Mercys");
+        Redis::sadd("names", "Mercys");
+        Redis::sadd("names", "Thona");
+        Redis::sadd("names", "Thona");
+
+        $response = Redis::smembers("names");
+        self::assertEquals(["Farel", "Mercys", "Thona"], $response);
+    }
+
+    public function testSortedSet()
+    {
+
+        Redis::del("names");
+
+        Redis::zadd("names", 100, "Farel");
+        Redis::zadd("names", 100, "Farel");
+        Redis::zadd("names", 85, "Mercys");
+        Redis::zadd("names", 85, "Mercys");
+        Redis::zadd("names", 95, "Thona");
+        Redis::zadd("names", 95, "Thona");
+
+        $response = Redis::zrange("names", 0, -1);
+        self::assertEquals(["Mercys", "Thona", "Farel"], $response);
+    }
+
+    public function testHash()
+    {
+        Redis::del("user:1");
+
+        Redis::hset("user:1", "name", "Farel");
+        Redis::hset("user:1", "email", "farel@localhost");
+        Redis::hset("user:1", "age", 30);
+
+        $response = Redis::hgetall("user:1");
+        self::assertEquals([
+            "name" => "Farel",
+            "email" => "farel@localhost",
+            "age" => "18"
+        ], $response);
+    }
+
+
 }
